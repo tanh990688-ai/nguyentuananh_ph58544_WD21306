14defa208b4dcddb99980e0f7ee6ccb55cfd358a
tuananh184 <tanh990688@gmail.com>
2025-12-12 11:32:22 +0700

    thithu

--- Thiquatrinhlap3/Thiquatrinhlap3/Program.c
+++ Thiquatrinhlap3/Thiquatrinhlap3/Program.c
@@ -4,101 +4,173 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void bai1() {
-    char ten1[50], ten2[50], ten3[50];bai1
-    int sl1, sl2, sl3;
+//void bai1() {
+//    char ten1[50], ten2[50], ten3[50];bai1
+//    int sl1, sl2, sl3;
+//
+//    printf("\n--- BAI 1: THONG TIN SAN PHAM ---\n");
+//
+//    printf("Nhap ten san pham 1: ");
+//    fflush(stdin); gets(ten1);
+//    printf("Nhap so luong: ");
+//    scanf("%d", &sl1);
+//
+//    printf("Nhap ten san pham 2: ");
+//    fflush(stdin); gets(ten2);
+//    printf("Nhap so luong: ");
+//    scanf("%d", &sl2);
+//
+//    printf("Nhap ten san pham 3: ");
+//    fflush(stdin); gets(ten3);
+//    printf("Nhap so luong: ");
+//    scanf("%d", &sl3);
+//
+//    printf("\n--- Thong tin vua nhap ---\n");
+//    printf("1. %s - So luong: %d\n", ten1, sl1);
+//    printf("2. %s - So luong: %d\n", ten2, sl2);
+//    printf("3. %s - So luong: %d\n", ten3, sl3);
+//}
+//
+//void bai2() {
+//    int n;
+//    printf("\n--- BAI 2: CAC SO CHIA HET CHO 3 ---\n");
+//    printf("Nhap so nguyen duong n: ");
+//    scanf("%d", &n);
+//
+//    printf("\nCac so nho hon %d chia het cho 3: ", n);
+//    int tong = 0, dem = 0;
+//    for (int i = 1; i < n; i++) {
+//        if (i % 3 == 0) {
+//            printf("%d ", i);
+//            tong += i;
+//            dem++;
+//        }
+//    }
+//
+//    if (dem > 0) {
+//        float tb = (float)tong / dem;
+//        printf("\nTrung binh cong cac so chia het cho 3 = %.2f\n", tb);
+//    }
+//    else {
+//        printf("\nKhong co so nao chia het cho 3!\n");
+//    }
+//}
+//
+//void bai3() {
+//    char ten[50], chucvu[50];
+//    int tuoi;
+//    float luong;
+//
+//    printf("\n--- BAI 3: THONG TIN NHAN VIEN ---\n");
+//
+//    printf("Nhap ten nhan vien: ");
+//    fflush(stdin); gets(ten);
+//    printf("Nhap tuoi: ");
+//    scanf("%d", &tuoi);
+//    printf("Nhap chuc vu: ");
+//    fflush(stdin); gets(chucvu);
+//    printf("Nhap luong: ");
+//    scanf("%f", &luong);
+//
+//    printf("\n--- Thong tin nhan vien ---\n");
+//    printf("Ten: %s\n", ten);
+//    printf("Tuoi: %d\n", tuoi);
+//    printf("Chuc vu: %s\n", chucvu);
+//    printf("Luong: %.2f\n", luong);
+//}
+//
+//int main() {
+//    int chon;
+//
+//    do {
+//        printf("\n-------------------------Menu-------------------------\n");
+//        printf("1. Thong tin san pham\n");
+//        printf("2. Trung binh cong cac so chia het cho 3\n");
+//        printf("3. Thong tin Nhan vien\n");
+//        printf("0. Thoat\n");
+//
+//        printf("Moi chon: ");
+//        scanf("%d", &chon);
+//
+//        switch (chon) {
+//        case 1: bai1(); break;
+//        case 2: bai2(); break;
+//        case 3: bai3(); break;
+//        case 0: printf("Thoat chuong trinh...\n"); break;
+//        default: printf("Lua chon khong hop le! Moi nhap lai.\n");
+//        }
+//    } while (chon != 0);
+//
+//    return 0;
+//}
 
-    printf("\n--- BAI 1: THONG TIN SAN PHAM ---\n");
 
-    printf("Nhap ten san pham 1: ");
-    fflush(stdin); gets(ten1);
-    printf("Nhap so luong: ");
-    scanf("%d", &sl1);
-
-    printf("Nhap ten san pham 2: ");
-    fflush(stdin); gets(ten2);
-    printf("Nhap so luong: ");
-    scanf("%d", &sl2);
-
-    printf("Nhap ten san pham 3: ");
-    fflush(stdin); gets(ten3);
-    printf("Nhap so luong: ");
-    scanf("%d", &sl3);
-
-    printf("\n--- Thong tin vua nhap ---\n");
-    printf("1. %s - So luong: %d\n", ten1, sl1);
-    printf("2. %s - So luong: %d\n", ten2, sl2);
-    printf("3. %s - So luong: %d\n", ten3, sl3);
-}
-
-void bai2() {
-    int n;
-    printf("\n--- BAI 2: CAC SO CHIA HET CHO 3 ---\n");
-    printf("Nhap so nguyen duong n: ");
-    scanf("%d", &n);
-
-    printf("\nCac so nho hon %d chia het cho 3: ", n);
-    int tong = 0, dem = 0;
-    for (int i = 1; i < n; i++) {
-        if (i % 3 == 0) {
-            printf("%d ", i);
-            tong += i;
-            dem++;
-        }
-    }
-
-    if (dem > 0) {
-        float tb = (float)tong / dem;
-        printf("\nTrung binh cong cac so chia het cho 3 = %.2f\n", tb);
-    }
-    else {
-        printf("\nKhong co so nao chia het cho 3!\n");
-    }
-}
-
-void bai3() {
-    char ten[50], chucvu[50];
-    int tuoi;
-    float luong;
-
-    printf("\n--- BAI 3: THONG TIN NHAN VIEN ---\n");
-
-    printf("Nhap ten nhan vien: ");
-    fflush(stdin); gets(ten);
-    printf("Nhap tuoi: ");
-    scanf("%d", &tuoi);
-    printf("Nhap chuc vu: ");
-    fflush(stdin); gets(chucvu);
-    printf("Nhap luong: ");
-    scanf("%f", &luong);
-
-    printf("\n--- Thong tin nhan vien ---\n");
-    printf("Ten: %s\n", ten);
-    printf("Tuoi: %d\n", tuoi);
-    printf("Chuc vu: %s\n", chucvu);
-    printf("Luong: %.2f\n", luong);
-}
+struct ThuCung {
+    char ma[20];
+    char ten[50];
+    int namSinh;
+};
 
 int main() {
     int chon;
 
     do {
-        printf("\n-------------------------Menu-------------------------\n");
-        printf("1. Thong tin san pham\n");
-        printf("2. Trung binh cong cac so chia het cho 3\n");
-        printf("3. Thong tin Nhan vien\n");
+        printf("\n===== MENU =====\n");
+        printf("1. Thong tin thu cung\n");
+        printf("2. Tinh tong\n");
+        printf("3. Thong tin cua hang\n");
         printf("0. Thoat\n");
-
         printf("Moi chon: ");
         scanf("%d", &chon);
 
-        switch (chon) {
-        case 1: bai1(); break;
-        case 2: bai2(); break;
-        case 3: bai3(); break;
-        case 0: printf("Thoat chuong trinh...\n"); break;
-        default: printf("Lua chon khong hop le! Moi nhap lai.\n");
+        if (chon == 1) {
+            // -------- CHUC NANG 1 --------
+            struct ThuCung ds[3];
+            int namHienTai;
+            printf("\nNhap nam hien tai: ");
+            scanf("%d", &namHienTai);
+
+            for (int i = 0; i < 3; i++) {
+                printf("\nNhap thu cung thu %d:\n", i + 1);
+                printf("Ma thu cung: ");
+                scanf("%s", ds[i].ma);
+                printf("Ten thu cung: ");
+                scanf("%s", ds[i].ten);
+                printf("Nam sinh: ");
+                scanf("%d", &ds[i].namSinh);
+            }
+
+            printf("\n--- Thong tin vua nhap ---\n");
+            for (int i = 0; i < 3; i++) {
+                printf("\nMa: %s", ds[i].ma);
+                printf("\nTen: %s", ds[i].ten);
+                printf("\nNam sinh: %d", ds[i].namSinh);
+                printf("\nTuoi: %d\n", namHienTai - ds[i].namSinh);
+            }
+
         }
+        else if (chon == 2) {
+            // -------- CHUC NANG 2 --------
+            int n, tong = 0;
+
+            do {
+                printf("\nNhap 1 so nguyen duong n > 3: ");
+                scanf("%d", &n);
+            } while (n <= 3);
+
+            for (int i = 1; i <= n; i++) {
+                tong += i;
+            }
+
+            printf("\nTong tu 1 den %d = %d\n", n, tong);
+
+            if (tong % 5 == 0)
+                printf("Tong chia het cho 5\n");
+            else
+                printf("Tong KHONG chia het cho 5\n");
+
+        }
+
     } while (chon != 0);
 
     return 0;
